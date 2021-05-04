af805afbf561387bae8295e901c56aa80ad6956b
KathyLi <lkx626@gmail.com>
2021-05-03 23:05:22 -0700

    format.cpp
    Pushed changes for ElapsedTime

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,12 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+// Note to myself: long int 32 bits, int 16 bits
+string Format::ElapsedTime(long seconds) { 
+    int minutes =  seconds >= 60 ? (int)seconds/60 : 0;
+    int hours = minutes >= 60 ? minutes/60 : 0;
+    int remainSec = (int)seconds%60;
+    string outputTime = std::to_string(hours) + ":" + std::to_string(minutes)
+                         +  ":" + std::to_string(remainSec);
+    return outputTime; 
+}
