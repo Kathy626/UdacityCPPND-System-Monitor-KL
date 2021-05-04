a3656760b0cb980cf26836738e801d511c1fdf8d
KathyLi <lkx626@gmail.com>
2021-05-03 23:05:38 -0700

    process.h
    Pushed change for process.h

--- include/process.h
+++ include/process.h
@@ -2,22 +2,32 @@
 #define PROCESS_H
 
 #include <string>
+
 /*
 Basic class for Process representation
 It contains relevant attributes as shown below
 */
-class Process {
- public:
-  int Pid();                               // TODO: See src/process.cpp
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+class Process
+{
+public:
+    Process(int pID);
+    int Pid();                              // TODO: See src/process.cpp
+    std::string User();                     // TODO: See src/process.cpp
+    std::string Command();                  // TODO: See src/process.cpp
+    float CpuUtilization();                 // TODO: See src/process.cpp
+    std::string Ram();                      // TODO: See src/process.cpp
+    long int UpTime();                      // TODO: See src/process.cpp
+    bool operator<(Process const &a) const; // TODO: See src/process.cpp
 
-  // TODO: Declare any necessary private members
- private:
+    // TODO: Declare any necessary private members
+private:
+    int ProcessID_;
+    std::string user_;
+    std::string cmd_;
+    float CpuUsage_;
+    std::string ram_;
+    long int uptime_;
+
 };
 
 #endif
